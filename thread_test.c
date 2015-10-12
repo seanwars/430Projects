18a5c6bd5e30eea526c3cf68f8852f58d5097779
Shane Nelson <stnelso1@asu.edu>
2015-10-12 16:37:18 -0700

    initial commit

---
+++ thread_test.c
@@ -0,0 +1,42 @@
+//
+//  thread_test.c
+//  q
+//
+//  Created by Shane T. Nelson on 10/12/15.
+//  Copyright © 2015 Shane T. Nelson. All rights reserved.
+//
+
+#include "threads.h"
+
+TCB_t * RunQ; //global header pointer to TCB
+
+int global = 0; //global variable for testing
+//function for thread testing
+void f1()
+{
+	int local = 0;
+	while(1)
+	{
+		printf("Thread from F1: %d, %d", local++, global++);
+		sleep(1);	//slow down thread
+		yield();	//wait to be preempted
+	}
+}
+//function for thread testing
+void f2()
+{
+	int local = 0;
+	while(1)
+	{
+		printf("Thread from F2: %d, %d", local++, global++);
+		sleep(1);	//slow down thread
+		yield();	//wait to be preempted
+	}
+}
+
+void main()
+{
+	start_thread(f1);
+	start_thread(f2);
+	run();
+}
