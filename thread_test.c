852618e424d6ab5185fb68a7296dd4b078c391f3
Shane Nelson <stnelso1@asu.edu>
2015-10-12 20:10:35 -0700

    slight update

--- thread_test.c
+++ thread_test.c
@@ -2,13 +2,12 @@
 //  thread_test.c
 //  q
 //
+//  Created by Sean C Bennett on 10/12/15.
 //  Created by Shane T. Nelson on 10/12/15.
 //  Copyright © 2015 Shane T. Nelson. All rights reserved.
 //
 
 #include "threads.h"
-
-Queue * RunQ; //global header pointer to TCB
 
 int global = 0; //global variable for testing
 //function for thread testing
